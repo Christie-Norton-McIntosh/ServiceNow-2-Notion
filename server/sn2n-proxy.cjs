2b47e40d89c4c13cc15d0299b65c6daec915c182
Christie Norton-McIntosh <norton.mcintosh@gmail.com>
2025-10-03 00:35:55 -0500

    Revert "feat: use 💠 emoji as default page icon instead of external image"
    This reverts commit e6fce766f737c8e32eda0734ba056b5337841dc4.

--- server/sn2n-proxy.cjs
+++ server/sn2n-proxy.cjs
@@ -1662,8 +1662,10 @@ app.post("/api/W2N", async (req, res) => {
       parent: { database_id: payload.databaseId },
       properties: properties,
       icon: {
-        type: "emoji",
-        emoji: "💠",
+        type: "external",
+        external: {
+          url: "https://raw.githubusercontent.com/Christie-Norton-McIntosh/ServiceNow-2-Notion/main/ServiceNow-2-Notion/src/img/ServiceNow%20icon.png",
+        },
       },
       cover: {
         type: "external",
