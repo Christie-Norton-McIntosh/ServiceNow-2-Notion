cd2837df155bd0f24d44439892f6de1e1dace8cc
Christie Norton-McIntosh <norton.mcintosh@gmail.com>
2025-10-03 17:07:20 -0500

    chore: clean up project files - remove duplicate dir, backups, and log

--- ServiceNow-2-Notion/src/content/index.js
+++
@@ -1,5 +0,0 @@
-// Content Module Index - Re-exports all content processing modules
-
-export * from "./metadata-extractor.js";
-export * from "./content-extractor.js";
-export * from "./content-utils.js";
