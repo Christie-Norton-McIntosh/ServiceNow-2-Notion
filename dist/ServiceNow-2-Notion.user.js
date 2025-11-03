c5f17cca8fd9ec09fe99b452ad8d21ac3fcae3eb
Christie Norton-McIntosh <norton.mcintosh@gmail.com>
2025-11-02 21:43:25 -0600

    chore: build v10.0.2 userscript

--- dist/ServiceNow-2-Notion.user.js
+++ dist/ServiceNow-2-Notion.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ServiceNow-2-Notion
 // @namespace    https://github.com/Christie-Norton-McIntosh/ServiceNow-2-Notion
-// @version      10.0.1
+// @version      10.0.2
 // @description  Extract ServiceNow content and save to Notion via proxy server
 // @author       Norton-McIntosh
 // @match        https://*.service-now.com/*
@@ -25,7 +25,7 @@
 (function() {
     'use strict';
     // Inject runtime version from build process
-    window.BUILD_VERSION = "10.0.1";
+    window.BUILD_VERSION = "10.0.2";
 (function () {
 
   // Configuration constants and default settings
